6085661b27628587c46d19bc42143da490a139e9
ziennaa <manyakalra817@gmail.com>
2025-05-27 03:42:41 +0530

    Create I. Welcome for you with Conditions.cpp

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x, y;
+    cin>>x>>y;
+    if(x>=y){
+        cout<<"Yes"<<endl;
+    }else{
+        cout<<"No"<<endl;
+    }
+}
