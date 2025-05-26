2c92aa7cf95cf94cbd3b07c36fd3d6a8a2232df5
ziennaa <manyakalra817@gmail.com>
2025-05-27 03:54:27 +0530

    Create F. Digits Summation.cpp

---
+++ F. Digits Summation.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long x, y;
+    cin>>x>>y;
+    cout<<(x%10)+(y%10)<<endl;
+} 
+
+// first error : digit extraction
+// 2nd error : using int instead of long or long long
