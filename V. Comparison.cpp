59a23830512009adf5768ea0d7b8f8898c214d13
ziennaa <manyakalra817@gmail.com>
2025-05-27 04:05:42 +0530

    Create V. Comparison.cpp

---
+++ V. Comparison.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x, y;
+    char s;
+    cin>>x>>s>>y;
+    if(s=='>'){
+        if(x>y){
+            cout<<"Right"<<endl;
+        }else{
+            cout<<"Wrong"<<endl;
+        }
+    }else if (s=='<')
+    {
+       if(x<y){
+        cout<<"Right"<<endl;
+       }else{
+        cout<<"Wrong"<<endl;
+       }
+    }else if (s=='=')
+    {
+        if(x==y){
+            cout<<"Right"<<endl;
+        }else{
+            cout<<"Wrong"<<endl;
+        }
+    } 
+}
