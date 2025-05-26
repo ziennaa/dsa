bd1e70764dc37174301b168895db2af6f47ff641
ziennaa <manyakalra817@gmail.com>
2025-05-27 04:00:10 +0530

    Create L. The Brothers.cpp

---
+++ L. The Brothers.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string x, y;
+    cin>>x>>y;
+    string a, b;
+    cin>>a>>b;
+    if(y==b){
+        cout<<"ARE Brothers"<<endl;
+    }else{
+        cout<<"NOT"<<endl;
+    }
+}
