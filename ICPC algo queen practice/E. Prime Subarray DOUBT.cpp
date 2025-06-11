4bdac7d60071c8150919c2ab5ded3bd579d435ae
ziennaa <manyakalra817@gmail.com>
2025-06-11 05:37:49 +0530

    Create E. Prime Subarray DOUBT.cpp

---
+++ ICPC algo queen practice/E. Prime Subarray DOUBT.cpp
@@ -0,0 +1,32 @@
+/*Prime Subarray
+Contests
+>
+ICPC Algo Queen 2025 - Practice Contest 2
+>
+Prime Subarray
+Given an array arr of size N, find the number of subarrays whose sum is prime.
+
+The sum of all elements in arr <= 10^7
+
+Note - An array b is a subarray of an array a if b can be obtained from a by deletion of several (possibly, zero or all) elements from the beginning and several (possibly, zero or all) elements from the end. In particular, an array is a subarray of itself.
+
+Sample 0
+Input
+arr = [1, 2, 3]
+
+Output
+4
+
+Explanation
+The subarrays with sum as a prime number are [2], [3], [2,3] and [1,2].
+
+Sample 1
+Input
+arr = [1]
+
+Output
+0
+
+Explanation
+There are no subarrays with sum as a prime number.*/
+// above question DOUBT
