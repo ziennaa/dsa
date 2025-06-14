09ecea6fe280eef2fab557a4d14eced9060ffc1e
Manya Kalra <manyakalra817@gmail.com>
2025-06-15 02:03:09 +0530

    Create A. Upload more ram 800 greedy.cpp

---
+++ A. Upload more ram 800 greedy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n, k;
+        cin>>n>>k;
+        cout<<1+(n-1)*k<<endl;
+    }
+}
+
+// a+(n-1)d that's it no need to think that much
