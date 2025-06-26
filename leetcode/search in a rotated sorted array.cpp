49fedc4ba74bba4f27b4051f6496abc8c07c4929
Manya Kalra <manyakalra817@gmail.com>
2025-06-26 23:33:27 +0530

    Update search in a rotated sorted array.cpp

--- leetcode/search in a rotated sorted array.cpp
+++ leetcode/search in a rotated sorted array.cpp
@@ -23,3 +23,36 @@ public:
         return -1;  
     }
 };
+
+// tc not mnatching above
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else {
+                if (nums[mid] < target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+
+// here we sorted only left halft and right half
+/*
+4567 not here
+so move to right half 01233
+    founded here!!!*/
