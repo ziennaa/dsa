660bfc5115f929ef9e149fa2deed666afde08844
Manya Kalra <manyakalra817@gmail.com>
2025-06-26 07:35:54 +0530

    Create missing number.cpp

---
+++ leetcode/missing number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> freq(n + 1, 0);
+        for(int i=0; i<n; i++){
+            freq[nums[i]]++;
+        }
+        int c = 0;
+        for(int i=0; i<=n; i++){
+            if(freq[i]==0){
+                c = i;
+            }
+        }
+        return c;
+    }
+};
+
+// not matching tc and sc requirement
+// need to find optimal sol and get back to this
