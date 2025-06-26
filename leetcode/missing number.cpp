c591b2a05b776edbf12cd8e28da9f075d44dcd8d
Manya Kalra <manyakalra817@gmail.com>
2025-06-26 21:32:58 +0530

    Update missing number.cpp

--- leetcode/missing number.cpp
+++ leetcode/missing number.cpp
@@ -18,3 +18,16 @@ public:
 
 // not matching tc and sc requirement
 // need to find optimal sol and get back to this
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int total = (n*(n+1))/2;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+        }
+        return total-sum;
+    }
+};
