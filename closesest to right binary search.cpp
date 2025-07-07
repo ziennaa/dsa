16f39e781eefe752cf3c2ceb42155cbc2bcd07ec
Manya Kalra <manyakalra817@gmail.com>
2025-07-07 13:37:57 +0530

    Create closesest to right binary search.cpp

---
+++ closesest to right binary search.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll n, k;
+    cin>>n>>k;
+    ll a[n];
+    for(ll i=0; i<n; i++){
+        cin>>a[i];
+    }
+    for(ll i=0; i<k; i++){
+        int p;
+        cin>>p;
+        int l=-1; //a[l]<=x
+        int r=n; //a[r]>x
+        while(l+1<r){
+            int m = (r+l)/2;
+            if(a[m]>=p){
+                r=m;
+            }else{
+                l=m;
+            }
+        }
+        cout<<r+1<<'\n';
+    }
+}
