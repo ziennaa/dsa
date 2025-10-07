2ac5494ed72f3270a84f3050211d951f86d54d8a
Manya Kalra <manyakalra817@gmail.com>
2025-10-07 18:33:52 +0530

    Add function to count distinct prime factors
    Implement distinctPrimeFactors function to count unique prime factors from an array.

--- leetcode/2521. Distinct Prime Factors of Product of Array.cpp
+++ leetcode/2521. Distinct Prime Factors of Product of Array.cpp
@@ -31,3 +31,22 @@ idea:
 3. add them in a map
 4. count the size of the map
 */
+class Solution {
+public:
+    int distinctPrimeFactors(vector<int>& nums) {
+        int mp[1000001] = {0};
+        for(auto p : nums){
+            for(auto i=2; i*i<=p; i++){
+                while(p%i==0){
+                    mp[i]++;
+                    p/=i;
+                }
+            }
+            if(p > 1) mp[p] = 1;
+        }
+        int count = 0;
+        for(int i = 2; i <= 1000; i++) if(mp[i]) count++;
+        return count;
+    }
+};
+// stil can be optimised further
