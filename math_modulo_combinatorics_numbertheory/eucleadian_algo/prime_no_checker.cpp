9979a04c0911f7a7433f2e92d4e208f3f2f382c7
Manya Kalra <manyakalra817@gmail.com>
2025-12-20 19:28:22 +0530

    Add prime number checker in C++

---
+++ math_modulo_combinatorics_numbertheory/eucleadian_algo/prime_no_checker.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+bool isPrime(int n){
+    if(n<=1) return false;
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+signed main(){
+    int n;
+    cin>>n;
+    if(isPrime(n)) cout<<"yes "<<n<<" is prime \n";
+    else cout<<"no "<<n<<" is not prime \n";
+    return 0;
+}
